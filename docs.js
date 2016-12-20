7cc199a1d74ee36e8c53789f4f23163facca26e3
tkellehe <mitchell.kelleher@gmail.com>
2016-12-20 18:17:24 -0500

    Update docs.js

--- docs.js
+++ docs.js
@@ -1,10 +1,41 @@
-(function(global, $){
+(function(global, $, noodel){
 
+var nbs = String.fromCharCode(160);
+  
+var nbsRemoveRegex = new RegExp(nbs,"g");
+function nbsRemove(string) {
+  return string.replace(nbsRemoveRegex, " ");
+};
+var nbsAddRegex = new RegExp(" ","g");
+function nbsRemove(string) {
+  return string.replace(nbsAddRegex, nbs);
+};
+  
 $(function(){
+
 /// Handle all editors in the window.
 $(".noodel-exec").each(function(){
-  console.log(this);
-});
+  var $this = $(this),
+      $editor = $this.children(".noodel-editor")[0],
+      $input = $this.children(".noodel-editor")[0],
+      $output = $this.children(".noodel-editor")[0],
+      $button = $this.children("button")[0];
+  
+  $button.text("RUN");
+  
+  function clickRun() {
+    
+    $button.text("STOP").unbind("click").click(clickStop);
+  };
+  function clickStop() {
+    
+    $button.text("STOP").unbind("click").click(clickRun);
+  };
+  
+  $button.click(clickRun);
 });
 
-})(this, this.$)
+
+});
+
+})(this, this.$, this.noodel)
