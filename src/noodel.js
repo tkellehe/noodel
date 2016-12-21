081a7379e13a64742b0f4bd5ae02855089880317
tkellehe <mitchell.kelleher@gmail.com>
2016-12-20 23:10:02 -0500

    Update noodel.js

--- src/noodel.js
+++ src/noodel.js
@@ -1,18 +1,24 @@
 (function(global, Pipe, Command, Token, Path, CHAR, NUMBER, STRING, ARRAY){
 
-function move_inputs(tkn, path) {
+function in_to_out(tkn, path) {
   tkn.outputs.pipe(tkn.inputs);
+}
+function out_to_in(tkn, path) {
+  if(tkn.parent) tkn.inputs.pipe(tkn.parent.outputs);
 }
 
 /// NOPs
 Command.commands[" "] = function(cmd) {
-  cmd.exec = move_inputs;
+  cmd.exec = out_to_in;
+  cmd.exec = in_to_out;
 };
 Command.commands["\t"] = function(cmd) {
-  cmd.exec = move_inputs;
+  cmd.exec = out_to_in;
+  cmd.exec = in_to_out;
 };
 Command.commands["\n"] = function(cmd) {
-  cmd.exec = move_inputs;
+  cmd.exec = out_to_in;
+  cmd.exec = in_to_out;
 };
 
 Path.prototype.printify = function() {
