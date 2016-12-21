2580e156b18bb664a2d5bc722aef1946efb18d8f
tkellehe <mitchell.kelleher@gmail.com>
2016-12-21 00:12:07 -0500

    Update noodel.js

--- src/noodel.js
+++ src/noodel.js
@@ -40,6 +40,59 @@ Command.commands[char_codes[239]] = function(cmd) {
   
   cmd.exec = in_to_out;
 };
+Command.commands[char_codes[237]] = function(cmd) {
+  cmd.exec = out_to_in;
+  
+  var old = cmd.tokenize;
+  cmd.tokenize = function(tkn) {
+    tkn.content = "";
+    for(var i = tkn.end+1; i < tkn.code.length && CHAR.is_printable(tkn.code[i]); ++i)
+      tkn.content += tkn.code[i];
+    tkn.end = tkn.literal.length + tkn.start + tkn.content.length;
+    return old.call(this, tkn);
+  };
+  
+  cmd.exec = function(tkn, path) {
+    var a = [];
+    for(var i = 0; i < tkn.content.length; ++i)
+      a.push(new CHAR(tkn.content[i]));
+    tkn.outputs.back(new ARRAY(a));
+  }
+  
+  cmd.exec = in_to_out;
+};
+  
+/// Operands
+Command.commands[char_codes[251]] = function(cmd) {
+  cmd.exec = out_to_in;
+  
+  cmd.exec = function(tkn, path) {
+    var lhs = tkn.inputs.front();
+    if(lhs) {
+      var rhs = tkn.inputs.front();
+      if(rhs) {
+        tkn.outputs.back(lhs.add(rhs, tkn));
+      } else tkn.outputs.back(lhs);
+    }
+  }
+  
+  cmd.exec = in_to_out;
+};
+Command.commands[char_codes[252]] = function(cmd) {
+  cmd.exec = out_to_in;
+  
+  cmd.exec = function(tkn, path) {
+    var lhs = tkn.inputs.front();
+    if(lhs) {
+      var rhs = tkn.inputs.front();
+      if(rhs) {
+        tkn.outputs.back(lhs.sub(rhs, tkn));
+      } else tkn.outputs.back(lhs);
+    }
+  }
+  
+  cmd.exec = in_to_out;
+};
 
 Path.prototype.printify = function() {
   return (new ARRAY(this.outputs.__array__)).printify();
