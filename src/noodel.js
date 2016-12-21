804b3ef17eb3110b543df2163e5183576d86d1c5
tkellehe <mitchell.kelleher@gmail.com>
2016-12-21 00:01:19 -0500

    Update noodel.js

--- src/noodel.js
+++ src/noodel.js
@@ -1,4 +1,4 @@
-(function(global, Pipe, Command, Token, Path, CHAR, NUMBER, STRING, ARRAY){
+(function(global, Pipe, Command, Token, Path, char_codes, CHAR, NUMBER, STRING, ARRAY){
 
 function in_to_out(tkn, path) {
   tkn.outputs.pipe(tkn.inputs);
@@ -20,6 +20,24 @@ Command.commands["\n"] = function(cmd) {
   cmd.exec = out_to_in;
   cmd.exec = in_to_out;
 };
+  
+/// Literals
+Command.commands[char_codes[239]] = function(cmd) {
+  cmd.exec = out_to_in;
+  
+  cmd.tokenize = function(tkn) {
+    tkn.content = "";
+    for(var i = tkn.end+1; i < tkn.code.length && CHAR.is_printable(tkn.code[i]); ++i)
+      tkn.content += tkn.code[i];
+    tkn.end = tkn.literal.length + tkn.start + tkn.content.length;
+  };
+  
+  cmd.exec = function(tkn, path) {
+    tkn.outputs.back(new STRING(tkn.content));
+  }
+  
+  cmd.exec = in_to_out;
+};
 
 Path.prototype.printify = function() {
   return (new ARRAY(this.outputs.__array__)).printify();
@@ -27,4 +45,4 @@ Path.prototype.printify = function() {
   
 global.noodel = function noodel(code) { if(typeof code === "string" && code.length) return new Path(code) };
 
-})(this, this.Pipe, this.Command, this.Token, this.Path, this.types.CHAR, this.types.NUMBER, this.types.STRING, this.types.ARRAY)
+})(this, this.Pipe, this.Command, this.Token, this.Path, this.char_codes, this.types.CHAR, this.types.NUMBER, this.types.STRING, this.types.ARRAY)
