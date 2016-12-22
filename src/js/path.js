57756d2e388ed1e798bd91ead649c3d7aac5d05e
tkellehe <mitchell.kelleher@gmail.com>
2016-12-21 21:43:40 -0500

    Update path.js

--- src/js/path.js
+++ src/js/path.js
@@ -11,6 +11,7 @@ function Path(code) {
   this.onends = [];
   this.onstops = [];
   this.timeout = undefined;
+  this.rate = 1;
   
   var self = this;
   function invoke_onsteps() { for(var i = 0, l = self.onsteps.length; i < l; ++i) self.onsteps[i].call(self) };
@@ -66,7 +67,7 @@ Path.prototype.exec = function() {
   var self = this;
   (function loop(){
     if(self.step()) {
-      self.timeout = setTimeout(loop, 1);
+      self.timeout = setTimeout(loop, self.rate);
     } else {
       self.timeout = undefined;
       self.onend();
