39a46303f9a58e44327ebedbd50082f7c18e58b3
tkellehe <mitchell.kelleher@gmail.com>
2016-12-27 11:00:37 -0500

    Update noodel.js

--- src/noodel.js
+++ src/noodel.js
@@ -11,4 +11,18 @@ noodel.commandify = function(cmd) {
   return new RegExp("^(" + cmd + ")$");
 };
 
+noodel.in_to_out = function(path) {
+  this.tkn.outputs.pipe(this.tkn.inputs);
+};
+noodel.out_to_in = function(path) {
+  if(this.tkn.parent) this.tkn.inputs.pipe(this.tkn.parent.outputs);
+};
+
+//------------------------------------------------------------------------------------------------------------
+/// NOPs
+Command.add(noodel.commandify("[ \t\n]"), function(cmd) {
+  cmd.exec = noodel.out_to_in;
+  cmd.exec = noodel.in_to_out;
+});
+
 })(this, this.Pipe, this.Command, this.Token, this.Path, this.characters, this.types.NUMBER, this.types.STRING, this.types.ARRAY)
