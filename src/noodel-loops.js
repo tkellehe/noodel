613d6020403f6231af9a020b94d1a104b3d8d903
tkellehe <mitchell.kelleher@gmail.com>
2016-12-27 11:08:20 -0500

    Create noodel-loops.js

---
+++ src/noodel-loops.js
@@ -0,0 +1,183 @@
+(function(global, noodel, Pipe, Command, Token, Path, characters, NUMBER, STRING, ARRAY){
+
+//------------------------------------------------------------------------------------------------------------
+/// Loops.
+//------------------------------------------------------------------------------------------------------------
+  
+//------------------------------------------------------------------------------------------------------------
+// Contiously loops the code following it up to a new line.
+Command.add(noodel.commandify(characters.correct("ḷ")), function(cmd) {
+  cmd.exec = noodel.out_to_in;
+  cmd.exec = function(path) {
+    this.tkn.inputs.pipe(this.tkn.sub_path.end.outputs);
+  }
+  
+  var old = cmd.tokenize;
+  cmd.tokenize = function() {
+    var tkn = this.tkn;
+    tkn.content = "";
+    for(var i = tkn.end+1; i < tkn.code.length && tkn.code[i] !== "\n"; ++i)
+      tkn.content += tkn.code[i];
+    if(tkn.code[i] === "\n") tkn.content += "\n";
+    tkn.end = tkn.literal.length + tkn.start + tkn.content.length - 1;
+    tkn.sub_path = new Path(tkn.content, tkn);
+    tkn.branches.front(tkn.sub_path.start);
+    tkn.sub_path.end.branches.front(tkn);
+    
+    tkn.next = function() { return tkn.sub_path.start };
+    
+    return old.call(this);
+  };
+  
+  cmd.exec = noodel.in_to_out;
+});
+
+//------------------------------------------------------------------------------------------------------------
+// Loops the given code up to a new line based off of the integerified value in the pipe which is removed.
+Command.add(noodel.commandify(characters.correct("Ḷ")), function(cmd) {
+  cmd.exec = noodel.out_to_in;
+  cmd.exec = function(path) {
+    var tkn = this.tkn;
+    if(tkn.count === undefined) {
+      tkn.next = function() { return tkn.sub_path.start };
+      var f = tkn.inputs.front();
+      if(f) {
+        f = f.integerify();
+        tkn.count = f.valueify();
+      } else tkn.count = 0;
+    }
+    if(tkn.count-- < 1)
+    {
+      tkn.count = undefined;
+      // Have to make sure account for if the end if the sub_path is the end of the prgm.
+      tkn.next = function() { var f = tkn.branches.first(); return f === tkn.sub_path.start ? undefined : f };
+    }
+    this.tkn.inputs.pipe(this.tkn.sub_path.end.outputs);
+  }
+  
+  var old = cmd.tokenize;
+  cmd.tokenize = function() {
+    var tkn = this.tkn;
+    tkn.content = "";
+    for(var i = tkn.end+1; i < tkn.code.length && tkn.code[i] !== "\n"; ++i)
+      tkn.content += tkn.code[i];
+    if(tkn.code[i] === "\n") tkn.content += "\n";
+    tkn.end = tkn.literal.length + tkn.start + tkn.content.length - 1;
+    tkn.sub_path = new Path(tkn.content, tkn);
+    tkn.branches.front(tkn.sub_path.start);
+    tkn.sub_path.end.branches.front(tkn);
+    
+    return old.call(this);
+  };
+  
+  cmd.exec = noodel.in_to_out;
+});
+
+//------------------------------------------------------------------------------------------------------------
+/// Breaks out of a looping command.
+Command.add(noodel.commandify(characters.correct("ḅ")), function(cmd) {
+  cmd.exec = noodel.out_to_in;
+  
+  var old = cmd.tokenize;
+  cmd.tokenize = function() {
+    var tkn = this.tkn, p = tkn.parent;
+    while(p.literal !== characters.correct("ḷ") &&
+          p.literal !== characters.correct("Ḷ")) {
+      p = p.parent;
+    }
+    
+    tkn.looper = p;
+    
+    tkn.next = function() {
+      var next = tkn.looper.branches.first();
+      return next === tkn.looper.sub_path.start ? undefined : next
+    };
+    
+    return old.call(this);
+  };
+  
+  cmd.exec = noodel.in_to_out;
+  
+  cmd.exec = function(path) {
+    if(this.tkn.next()) this.tkn.looper.outputs.pipe(this.tkn.outputs);
+  }
+});
+
+//------------------------------------------------------------------------------------------------------------
+// Delay for number of steps.
+Command.add(noodel.commandify(characters.correct("ḍ"), "\\d+"), function(cmd) {
+  cmd.exec = noodel.out_to_in;
+  
+  cmd.exec = function(path) {
+    var tkn = this.tkn;
+    if(!tkn.ran) {
+      tkn.old_next = tkn.next;
+      tkn.next = function() { return tkn };
+      tkn.ran = true;
+      tkn.old_rate = path.rate;
+      path.rate = +tkn.params[0];
+    } else {
+      tkn.ran = false;
+      tkn.next = tkn.old_next;
+      path.rate = tkn.old_rate;
+    }
+  }
+  
+  cmd.exec = noodel.in_to_out;
+});
+
+//------------------------------------------------------------------------------------------------------------
+// Delay for number of steps using fractions
+Command.add(new RegExp("^("+characters.correct("ḍ")+")(\\d*)/(\\d*)$"), function(cmd) {
+  cmd.exec = noodel.out_to_in;
+  
+  cmd.exec = function(path) {
+    var tkn = this.tkn;
+    if(!tkn.ran) {
+      tkn.old_next = tkn.next;
+      tkn.next = function() { return tkn };
+      tkn.ran = true;
+      tkn.old_rate = path.rate;
+      var num = 1000, den = 1;
+      if(tkn.params[0].length) {
+        num *= +tkn.params[0];
+      }
+      if(tkn.params[1].length) {
+        den = +tkn.params[1];
+      }
+      path.rate = Math.floor(num / den);
+    } else {
+      tkn.ran = false;
+      tkn.next = tkn.old_next;
+      path.rate = tkn.old_rate;
+    }
+  }
+  
+  cmd.exec = noodel.in_to_out;
+});
+
+//------------------------------------------------------------------------------------------------------------
+// Delay for number of steps using decimals
+Command.add(new RegExp("^("+characters.correct("ḍ")+")(\\d*)\\.(\\d*)$"), function(cmd) {
+  cmd.exec = noodel.out_to_in;
+  
+  cmd.exec = function(path) {
+    var tkn = this.tkn;
+    if(!tkn.ran) {
+      tkn.old_next = tkn.next;
+      tkn.next = function() { return tkn };
+      tkn.ran = true;
+      tkn.old_rate = path.rate;
+      var num = (+("0"+tkn.params[0]+"."+tkn.params[1]+"0")) * 1000;
+      path.rate = Math.floor(num);
+    } else {
+      tkn.ran = false;
+      tkn.next = tkn.old_next;
+      path.rate = tkn.old_rate;
+    }
+  }
+  
+  cmd.exec = noodel.in_to_out;
+});
+
+})(this, this.noodel, this.Pipe, this.Command, this.Token, this.Path, this.characters, this.types.NUMBER, this.types.STRING, this.types.ARRAY)
