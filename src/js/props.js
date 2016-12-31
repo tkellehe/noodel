3280094e455a33753590bfa136fa0ab147921c52
tkellehe <mitchell.kelleher@gmail.com>
2016-12-31 15:07:35 -0500

    Update props.js

--- src/js/props.js
+++ src/js/props.js
@@ -8,6 +8,8 @@ function Props() {
   }
   the_props.__props__ = {};
   the_props.clear = function() { the_props.__props__ = {}; return the_props };
+  the_props.get = function(name) { return the_props.__props__[name] };
+  the_props.set = function(name, value) { the_props[name] = value; return the_props; };
   return the_props;
 }
 
