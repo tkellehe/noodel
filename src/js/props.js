a1ec76e64094fa92aea5a7b4b7edd340d4d73de5
tkellehe <mitchell.kelleher@gmail.com>
2016-12-31 14:58:55 -0500

    Create props.js

---
+++ src/js/props.js
@@ -0,0 +1,16 @@
+(function(global){
+
+function Props() {
+  function the_props(name, value) {
+    if(arguments.length === 1) return the_props.__props__[name]
+    the_props.__props__[name] = value;
+    return the_props;
+  }
+  the_props.__props__ = {};
+  the_props.clear = function() { the_props.__props__ = {}; return the_props };
+  return the_props;
+}
+
+global.make_props = Props;
+
+})(this)
