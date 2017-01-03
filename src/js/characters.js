fc566704c6be09d2f7c6f65aa8024e90cf0af532
tkellehe <mitchell.kelleher@gmail.com>
2017-01-03 08:59:47 -0500

    Update characters.js

--- src/js/characters.js
+++ src/js/characters.js
@@ -320,6 +320,46 @@ characters.compress_bitpack = function(N, string) {
   return characters.debitify_string(all_bits);
 };
   
+characters.decompress_bitpack = function(N, compressed) {
+  // If N is 7, then just use the basic decompession.
+  if(N === 7) return characters.decompress_basic(compressed);
+  
+  var all_bits = characters.bitify_string(compressed), res = "";
+  
+  // Loop through each 8bit chunk in order to look at each character to find the packed characters.
+  for(var i = 0; i < all_bits.length; i += 8) {
+
+    // Grab the bits that we know holds a valid character.
+    var packed_bits = all_bits.slice(i + (8 - N), i + 8);
+    
+    // Fill it with zeros.
+    for(var c = 8 - N; c--;) packed_bits.unshift(0);
+    res += characters.debitify_char(packed_bits);
+
+    // If on the last character that could be holding a packed character.
+    if(((i/8)+1)%(N+1) === 0) {
+      // Loop back through each character that hold a packed character and get all of the characters packed.
+      for(var k = 1; k < (8 - N); ++k) {
+        var bits = [], offset = i - (N*8);
+        // If the flag bit is set, then the next N characters hold a packed character.
+        if(all_bits[offset+k] === 1) {
+          // Fill it with zeros.
+          for(var c = 8 - N; c--;) bits.push(0);
+          // Get the packed bits.
+          for(var j = 0; j < N; ++j) {
+            bits.push(all_bits[offset+k+((j+1)*8)]);
+          }
+          res += characters.debitify_char(bits);
+        } else {
+          // Can exit because no others should be one.
+          break;
+        }
+      }
+    }
+  }
+  return res;
+};
+  
 characters.compress_occur = function(s) {
   var key = "", res = "";
   
@@ -343,42 +383,10 @@ characters.compress_occur = function(s) {
 };
 
 characters.decompress_occur = function(key, compressed) {
-  var res = "";
   key = characters.decompress_basic(key);
   
-  var max_num_bits = (key.length - 1).toString(2).length;
-  var all_bits = characters.bitify_string(compressed), res = "";
-  
-  if(max_num_bits === 7) {
-    res = characters.decompress_basic(compressed);
-  } else {
-    var N = max_num_bits;
-    for(var i = 0; i < all_bits.length; i += 8) {
-      
-      var packed_bits = all_bits.slice(i + (8 - N), i + 8);
-      // Fill it with zeros.
-      for(var c = 8 - N; c--;) packed_bits.unshift(0);
-      res += characters.debitify_char(packed_bits);
-      
-      if(((i/8)+1)%(N+1) === 0) {
-        // Reached the last char that has chars.
-        for(var k = 1; k < (8 - N); ++k) {
-          var bits = [], o = i - (N*8);
-          if(all_bits[o+k] === 1) {
-            // Fill it with zeros.
-            for(var c = 8 - N; c--;) bits.push(0);
-            for(var j = 0; j < N; ++j) {
-              bits.push(all_bits[o+k+((j+1)*8)]);
-            }
-            res += characters.debitify_char(bits);
-          } else {
-            // Can exit because no others should be one.
-            break;
-          }
-        }
-      }
-    }
-  }
+  var max_num_bits = (key.length - 1).toString(2).length,
+      res = characters.decompress_bitpack(max_num_bits, compressed);
   
   var decompressed = "";
   
