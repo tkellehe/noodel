ac43cfe3f490daedec9e82f47d6b1c7780ed09b1
tkellehe <mitchell.kelleher@gmail.com>
2017-01-05 15:35:11 -0500

    Update noodel-basic_print.js

--- src/noodel-basic_print.js
+++ src/noodel-basic_print.js
@@ -72,34 +72,5 @@ Command.add(0, noodel.commandify(characters.correct("Ñ")), function(cmd) {
     }
   }
 });
-  
-//------------------------------------------------------------------------------------------------------------
-// Generates a random integer.
-Command.add(0, noodel.commandify(characters.correct("ṛ")), function(cmd) {
-  cmd.exec = noodel.out_to_in;
-  
-  cmd.exec = function(path) {
-    var f = path.top();
-    if(f) {
-      if(f.type === "NUMBER") {
-        var max, min;
-        var g = path.top();
-        if(g) {
-          max = Math.max(f.value, g.value);
-          min = Math.min(f.value, g.value);
-        } else {
-          max = Math.max(f.value, 0);
-          min = Math.min(f.value, 0);
-        }
-        path.top(new NUMBER(noodel.random_int(min, max)));
-      } else {
-        path.top(f);
-        path.top(new NUMBER(noodel.random_int(0, f.length() - 1)));
-      }
-    }
-  }
-  
-  cmd.exec = noodel.in_to_out;
-});
 
 })(this, this.noodel, this.Pipe, this.Command, this.Token, this.Path, this.characters, this.NUMBER, this.STRING, this.ARRAY)
