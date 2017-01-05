b9e966c77d005cde7dca7024bfa23508a0ab96fe
tkellehe <mitchell.kelleher@gmail.com>
2017-01-04 22:51:13 -0500

    Update noodel-literals.js

--- src/noodel-literals.js
+++ src/noodel-literals.js
@@ -66,12 +66,42 @@ Command.add(0, noodel.commandify(characters.correct("”"), characters.regex.a_c
 });
   
 //------------------------------------------------------------------------------------------------------------
-// Handles basic compressed string literals of printable characters.
-Command.add(1, noodel.commandify(characters.regex.a_compressable + "+", characters.correct("’"), characters.regex.a_compressable + "+"), function(cmd) {
+// Handles occur compressed string literals of printable characters.
+Command.add(1, noodel.commandify(characters.regex.a_compressable + "+", characters.correct("‘"), characters.regex.a_compressable + "+"), function(cmd) {
   cmd.exec = noodel.out_to_in;
   
   cmd.exec = function(path) {
     this.tkn.outputs.back(new STRING(this.tkn.params[0]));
+  }
+  
+  var old = cmd.tokenize;
+  cmd.tokenize = function() {
+    this.tkn.params[0] = characters.decompress_occur(this.tkn.params[0], this.tkn.params[1]);
+    
+    return old.call(this);
+  };
+  
+  cmd.exec = noodel.in_to_out;
+});
+
+//------------------------------------------------------------------------------------------------------------
+// Creates and array of strings from each compressable characters following it.
+Command.add(0, noodel.commandify(characters.regex.a_compressable + "+", characters.correct("’"), characters.regex.a_compressable + "+"), function(cmd) {
+  cmd.exec = noodel.out_to_in;
+  
+  cmd.exec = function(path) {
+    var a = this.tkn.params[0].split(characters.correct("ð"));
+    if(a.length === 1) {
+      a = [];
+      for(var i = 0; i < this.tkn.params[0].length; ++i)
+        a.push(new STRING(this.tkn.params[0][i]));
+      this.tkn.outputs.back(new ARRAY(a));
+    } else {
+      var s = [];
+      for(var i = 0; i < a.length; ++i)
+        s.push(new STRING(a[i]));
+      this.tkn.outputs.back(new ARRAY(s));
+    }
   }
   
   var old = cmd.tokenize;
