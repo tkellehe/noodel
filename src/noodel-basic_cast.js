526ae048947af1618b770c6310f99ee26939097e
tkellehe <mitchell.kelleher@gmail.com>
2017-01-05 17:11:16 -0500

    Update noodel-basic_cast.js

--- src/noodel-basic_cast.js
+++ src/noodel-basic_cast.js
@@ -7,165 +7,127 @@
 //------------------------------------------------------------------------------------------------------------
 /// Numberifies strings and arrays by elements if already a number it will flip the sign.
 Command.add(0, noodel.commandify(characters.correct("ɲ")), function(cmd) {
-  cmd.exec = noodel.out_to_in;
-  
   cmd.exec = function(path) {
-    var f = this.tkn.inputs.front();
+    var f = path.top();
     if(f) {
       if(f.type === "ARRAY") {
         for(var i = 0; i < f.length(); ++i) {
-          f.value[i] = f.access(i).numberify();
+          f.value[i] = f.value[i].numberify();
         }
-        this.tkn.outputs.back(f);
+        path.top(f);
       } else if(f.type === "NUMBER") {
-        this.tkn.outputs.back(new NUMBER(-1 * Math.abs(f.value)));
+        path.top(new NUMBER(-1 * Math.abs(f.value)));
       } else if(f.type === "STRING") {
-        var a = f.arrayify();
+        var a = string_null_break(f.value);
         for(var i = 0; i < a.length(); ++i) {
-          a.value[i] = a.value[i].numberify();
+          a.value[i] = (new STRING(a.value[i])).numberify();
         }
         if(a.length() === 0) {
           a = new NUMBER(0);
         } else if(a.length() === 1) {
           a = a.value[0]
         }
-        this.tkn.outputs.back(a);
+        path.top(a);
       }
-    } else {
-      noodel.make_error(new STRING("¤Expected¤something¤in¤the¤pipe."), path);
     }
   }
-  
-  cmd.exec = noodel.in_to_out;
 });
 
 //------------------------------------------------------------------------------------------------------------
 /// Stringifies the first item in the pipe.
 Command.add(0, noodel.commandify(characters.correct("ɲ")+"'"), function(cmd) {
-  cmd.exec = noodel.out_to_in;
-  
   cmd.exec = function(path) {
-    var f = this.tkn.inputs.front();
+    var f = path.top();
     if(f) {
       if(f.type === "ARRAY") {
         for(var i = 0; i < f.length(); ++i) {
-          f.value[i] = f.access(i).stringify();
+          f.value[i] = f.value[i].stringify();
         }
-        this.tkn.outputs.back(f);
-      } else {
-        this.tkn.outputs.back(f.stringify());
+        path.top(f);
+      } else if(f.type === "NUMBER") {
+        path.top(f.stringify());
+      } else if(f.type === "STRING") {
+        var s = "";
+        for(var i = f.length(); i--;) {
+          s += f.value[i];
+        }
+        path.top(new STRING(s));
       }
-    } else {
-      noodel.make_error(new STRING("¤Expected¤something¤in¤the¤pipe."), path);
     }
   }
-  
-  cmd.exec = noodel.in_to_out;
 });
 
 //------------------------------------------------------------------------------------------------------------
 /// Numberifies the first item in the pipe.
 Command.add(0, noodel.commandify(characters.correct("ɲ")+"#"), function(cmd) {
-  cmd.exec = noodel.out_to_in;
-  
   cmd.exec = function(path) {
-    var f = this.tkn.inputs.front();
+    var f = path.top();
     if(f) {
       if(f.type === "ARRAY") {
         for(var i = 0; i < f.length(); ++i) {
-          f.value[i] = f.access(i).numberify();
+          f.value[i] = f.value[i].numberify();
         }
-        this.tkn.outputs.back(f);
-      } else {
-        this.tkn.outputs.back(f.numberify());
+        path.top(f);
+      } else if(f.type === "STRING") {
+        path.top(f.numberify());
+      } else if(f.type === "NUMBER") {
+        path.top(new NUMBER(-1 * f.value));
       }
-    } else {
-      noodel.make_error(new STRING("¤Expected¤something¤in¤the¤pipe."), path);
     }
   }
-  
-  cmd.exec = noodel.in_to_out;
 });
 
 //------------------------------------------------------------------------------------------------------------
 /// Arrayifies the first item in the pipe.
 Command.add(0, noodel.commandify(characters.correct("ʋ")), function(cmd) {
-  cmd.exec = noodel.out_to_in;
-  
   cmd.exec = function(path) {
-    var f = this.tkn.inputs.front();
+    var f = path.top();
     if(f) {
-      var t = f.arrayify();
       if(f.type === "STRING") {
-        if(t.length() === 1) {
-          var a = [];
-          for(var i = 0; i < f.value.length; ++i) {
-            a.push(new STRING(f.value[i]));
-          }
-          t = new ARRAY(a);
-        }
+        path.top(f.arrayify());
+      } else if(f.type === "NUMBER") {
+        path.top(new NUMBER(Math.ceil(f.value)));
+      } else if(f.type === "ARRAY") {
+        // ???
       }
-      this.tkn.outputs.back(t);
-    } else {
-      noodel.make_error(new STRING("¤Expected¤something¤in¤the¤pipe."), path);
     }
   }
-  
-  cmd.exec = noodel.in_to_out;
 });
 
 //------------------------------------------------------------------------------------------------------------
 /// Collects all of the items in the pipe and creates an array for them.
 Command.add(0, noodel.commandify(characters.correct("œ")), function(cmd) {
-  cmd.exec = noodel.out_to_in;
-  
   cmd.exec = function(path) {
-    var pipe = new Pipe();
-    pipe.pipe(this.tkn.inputs);
-    if(pipe.length()) {
-      this.tkn.outputs.back(new ARRAY(pipe.__array__));
-    }
-  }
-  
-  cmd.exec = noodel.in_to_out;
+    var a = [];
+    while(path.first()) a.push(path.top());
+    if(a.length) path.top(new ARRAY(a));
 });
 
 //------------------------------------------------------------------------------------------------------------
 /// Collects the number of items specified by the count and creates an array for them.
 Command.add(0, noodel.commandify(characters.correct("œ"), "\\d+"), function(cmd) {
-  cmd.exec = noodel.out_to_in;
-  
   cmd.exec = function(path) {
     var a = [];
-    for(var i = +this.tkn.params[0]; i-- && this.tkn.inputs.length();) a.push(this.tkn.inputs.front());
+    for(var i = +this.tkn.params[0]; i-- && path.first();) a.push(path.top());
     if(a.length) {
-      this.tkn.outputs.back(new ARRAY(a));
+      path.top(new ARRAY(a));
     }
   }
-  
-  cmd.exec = noodel.in_to_out;
 });
 
 //------------------------------------------------------------------------------------------------------------
 /// Collects the number of items specified in the pipe and creates an array for them.
 Command.add(0, noodel.commandify(characters.correct("µ") + characters.correct("œ")), function(cmd) {
-  cmd.exec = noodel.out_to_in;
-  
   cmd.exec = function(path) {
-    var f = this.tkn.inputs.front();
-    if(!f) {
-      noodel.make_error(new STRING("Failed¤to¤find¤anything¤in¤the¤pipe."), path);
-      return;
-    }
-    
-    var a = [];
-    for(var i = f.integerify().valueify(); i-- && this.tkn.inputs.length();) a.push(this.tkn.inputs.front());
-    if(a.length) {
-      this.tkn.outputs.back(new ARRAY(a));
+    var f = path.top();
+    if(f) {
+      var a = [];
+      for(var i = f.integerify().value; i-- && path.first();) a.push(path.top());
+      if(a.length) {
+        path.top(new ARRAY(a));
+      }
     }
   }
-  
-  cmd.exec = noodel.in_to_out;
 });
 
-})(this, this.noodel, this.Pipe, this.Command, this.Token, this.Path, this.characters, this.types.NUMBER, this.types.STRING, this.types.ARRAY)
+})(this, this.noodel, this.Pipe, this.Command, this.Token, this.Path, this.characters, this.NUMBER, this.STRING, this.ARRAY)
