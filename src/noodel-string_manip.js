7920b553b2017acf25c440d954faa10db1964004
tkellehe <mitchell.kelleher@gmail.com>
2017-01-06 16:15:39 -0500

    Update noodel-string_manip.js

--- src/noodel-string_manip.js
+++ src/noodel-string_manip.js
@@ -6,7 +6,7 @@ function(global, noodel, Pipe, Command, Token, Path, characters, NUMBER, STRING,
 
 //------------------------------------------------------------------------------------------------------------
 /// Terminates the program.
-Command.add(0, noodel.commandify(characters.correct("ɲ") + "`"), function(cmd) {
+Command.add(0, noodel.commandify(characters.correct("ɲ") + characters.correct("ð")), function(cmd) {
   cmd.exec = function(path) {
     var f = path.top();
     if(f) {
