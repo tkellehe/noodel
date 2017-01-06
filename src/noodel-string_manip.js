1749e92cf7ab2110fa03e0699663262a36826a44
tkellehe <mitchell.kelleher@gmail.com>
2017-01-06 16:22:10 -0500

    Update noodel-string_manip.js

--- src/noodel-string_manip.js
+++ src/noodel-string_manip.js
@@ -4,17 +4,5 @@
 /// String Manipulation Commands.
 //------------------------------------------------------------------------------------------------------------
 
-//------------------------------------------------------------------------------------------------------------
-/// Terminates the program.
-Command.add(0, noodel.commandify(characters.correct("ɲ") + characters.correct("ð")), function(cmd) {
-  cmd.exec = function(path) {
-    var f = path.top();
-    if(f) {
-      if(f.type === "STRING") {
-        path.top(new STRING(string_null_break(f.value).join("")));
-      }
-    }
-  }
-});
 
 })(this, this.noodel, this.Pipe, this.Command, this.Token, this.Path, this.characters, this.NUMBER, this.STRING, this.ARRAY)
