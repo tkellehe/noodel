e3e28905e75045f27b5e150008c4f976e1c696d1
tkellehe <mitchell.kelleher@gmail.com>
2017-01-07 19:39:02 -0500

    Update noodel-string_manip.js

--- src/noodel-string_manip.js
+++ src/noodel-string_manip.js
@@ -4,5 +4,31 @@
 /// String Manipulation Commands.
 //------------------------------------------------------------------------------------------------------------
 
+//------------------------------------------------------------------------------------------------------------
+/// Converts element into lowercase.
+Command.add(0, noodel.commandify(characters.correct("ɲ") + "L"), function(cmd) {
+  cmd.exec = function(path) {
+    var f = path.top();
+    if(f) path.top(f.to_lowercase());
+  }
+});
+  
+//------------------------------------------------------------------------------------------------------------
+/// Converts element into uppercase.
+Command.add(0, noodel.commandify(characters.correct("ɲ") + "U"), function(cmd) {
+  cmd.exec = function(path) {
+    var f = path.top();
+    if(f) path.top(f.to_uppercase());
+  }
+});
+  
+//------------------------------------------------------------------------------------------------------------
+/// Switches case of item.
+Command.add(0, noodel.commandify(characters.correct("ɲ") + "S"), function(cmd) {
+  cmd.exec = function(path) {
+    var f = path.top();
+    if(f) path.top(f.switchcase());
+  }
+});
 
 })(this, this.noodel, this.Pipe, this.Command, this.Token, this.Path, this.characters, this.NUMBER, this.STRING, this.ARRAY)
