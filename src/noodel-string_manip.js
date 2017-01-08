35a30029893c404ed3f4ec8fa236f4de8cba22f5
tkellehe <mitchell.kelleher@gmail.com>
2017-01-08 12:42:38 -0500

    Update noodel-string_manip.js

--- src/noodel-string_manip.js
+++ src/noodel-string_manip.js
@@ -30,5 +30,27 @@ Command.add(0, noodel.commandify(characters.correct("ɲ") + "S"), function(cmd) 
     if(f) path.top(f.switchcase());
   }
 });
+  
+//------------------------------------------------------------------------------------------------------------
+/// Relocates an element in an object.
+Command.add(0, noodel.commandify(characters.correct("ṙ")), function(cmd) {
+  cmd.exec = function(path) {
+    var f = path.top();
+    if(f) {
+      var g = path.top();
+      if(g) {
+        var h = path.top();
+        if(h) {
+          path.top(h.relocate(f.integerify().value, g.integerify().value));
+        } else {
+          path.top(g);
+          path.top(h);
+        }
+      } else {
+        path.top(f);
+      }
+    }
+  }
+});
 
 })(this, this.noodel, this.Pipe, this.Command, this.Token, this.Path, this.characters, this.NUMBER, this.STRING, this.ARRAY)
