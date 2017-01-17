8b29a7ca69dd7f86814ff1e711e2ead8afd531e2
tkellehe <mitchell.kelleher@gmail.com>
2017-01-17 18:51:59 -0500

    Update characters.js

--- src/internal/characters.js
+++ src/internal/characters.js
@@ -1,7 +1,7 @@
 (function(global){
 
 function handleBug(s) { return s[s.length-1] };
-var int_to_char = [
+var int_to_nchar = [
 '\u00f0','\u00ac','\u00b6','\u00a4','\u0041','\u0042','\u0043','\u0044','\u0045','\u0046','\u0047','\u0048','\u0049','\u004a','\u004b','\u004c',
 '\u004d','\u004e','\u004f','\u0050','\u0051','\u0052','\u0053','\u0054','\u0055','\u0056','\u0057','\u0058','\u0059','\u005a','\u0061','\u0062',
 '\u0063','\u0064','\u0065','\u0066','\u0067','\u0068','\u0069','\u006a','\u006b','\u006c','\u006d','\u006e','\u006f','\u0070','\u0071','\u0072',
@@ -20,48 +20,40 @@ var int_to_char = [
 '\u00b9','\u00b2','\u00b3','\u2074','\u2075','\u2076','\u2077','\u2078','\u2079','\u207a','\u207b','\u207c','\u207d','\u207e','\u0020','\u000a'
 ];
 
-var regexified = [
-'\\u00f0','\\u00ac','\\u00b6','\\u00a4','\\u0041','\\u0042','\\u0043','\\u0044','\\u0045','\\u0046','\\u0047','\\u0048','\\u0049','\\u004a','\\u004b','\\u004c',
-'\\u004d','\\u004e','\\u004f','\\u0050','\\u0051','\\u0052','\\u0053','\\u0054','\\u0055','\\u0056','\\u0057','\\u0058','\\u0059','\\u005a','\\u0061','\\u0062',
-'\\u0063','\\u0064','\\u0065','\\u0066','\\u0067','\\u0068','\\u0069','\\u006a','\\u006b','\\u006c','\\u006d','\\u006e','\\u006f','\\u0070','\\u0071','\\u0072',
-'\\u0073','\\u0074','\\u0075','\\u0076','\\u0077','\\u0078','\\u0079','\\u007a','\\u002e','\\u0021','\\u003f','\\u002c','\\u0030','\\u0031','\\u0032','\\u0033',
-'\\u0034','\\u0035','\\u0036','\\u0037','\\u0038','\\u0039','\\u003a','\\u003b','\\u0022','\\u0027','\\u005f','\\u003c','\\u003d','\\u003e','\\u002a','\\u002b',
-'\\u002d','\\u002f','\\u005c','\\u0040','\\u0023','\\u0024','\\u0025','\\u0026','\\u005e','\\u007c','\\u0028','\\u0029','\\u005b','\\u005d','\\u007b','\\u007d',
-'\\u0060','\\u007e','\\u1ea0','\\u1e04','\\u1e0c','\\u1eb8','\\u1e24','\\u1eca','\\u1e32','\\u1e36','\\u1e42','\\u1e46','\\u1ecc','\\u1e5a','\\u1e62','\\u1e6c',
-'\\u1ee4','\\u1e7e','\\u1e88','\\u1ef4','\\u1e92','\\u0226','\\u1e02','\\u010a','\\u1e0a','\\u0116','\\u1e1e','\\u0120','\\u1e22','\\u0130','\\u013f','\\u1e40',
-'\\u1e44','\\u022e','\\u1e56','\\u1e58','\\u1e60','\\u1e6a','\\u1e86','\\u1e8a','\\u1e8e','\\u017b','\\u1ea1','\\u1e05','\\u1e0d','\\u1eb9','\\u1e25','\\u1ecb',
-'\\u1e33','\\u1e37','\\u1e43','\\u1e47','\\u1ecd','\\u1e5b','\\u1e63','\\u1e6d','\\u1ee5','\\u1e7f','\\u1e89','\\u1ef5','\\u1e93','\\u0227','\\u1e03','\\u010b',
-'\\u1e0b','\\u0117','\\u1e1f','\\u0121','\\u1e23','\\u0140','\\u1e41','\\u1e45','\\u022f','\\u1e57','\\u1e59','\\u1e61','\\u1e6b','\\u1e87','\\u1e8b','\\u1e8f',
-'\\u017c','\\u0181','\\u0187','\\u018a','\\u0191','\\u0193','\\u0198','\\u019d','\\u01a4','\\u01ac','\\u01b2','\\u0224','\\u0253','\\u0188','\\u0257','\\u0192',
-'\\u0260','\\u0266','\\u0199','\\u0271','\\u0272','\\u01a5','\\u02a0','\\u027c','\\u0282','\\u01ad','\\u028b','\\u0225','\\u00c6','\\u00c7','\\u00d0','\\u00d1',
-'\\u00d8','\\u0152','\\u00de','\\u00df','\\u00e6','\\u00e7','\\u0131','\\u0237','\\u00f1','\\u00f8','\\u0153','\\u00fe','\\u20ac','\\u00a2','\\u00a3','\\u00a5',
-'\\u2026','\\u00b5','\\u00a1','\\u00bf','\\u00d7','\\u00f7','\\u00a6','\\u00a9','\\u00ae','\\u00ab','\\u00bb','\\u2018','\\u2019','\\u201c','\\u201d','\\u00b0',
-'\\u00b9','\\u00b2','\\u00b3','\\u2074','\\u2075','\\u2076','\\u2077','\\u2078','\\u2079','\\u207a','\\u207b','\\u207c','\\u207d','\\u207e','\\u0020','\\u000a'
-];
-
-var char_to_int = {};
-
-// Corrects for when browsers place characters in front of the actual and populates char_to_int.
+var nchar_to_int = {};
 (function(){
-  for(var i = int_to_char.length; i--;) {
-    int_to_char[i] = handleBug(int_to_char[i]);
-    char_to_int[int_to_char[i]] = i;
+  for(var i = 0; i < 256; ++i) {
+    nchar_to_int[handleBug(int_to_nchar[i])] = i;
   }
-})();
+})()
 
-
+var regexified = [];
+(function(){
+  for(var i = 0; i < 256; ++i) {
+    regexified.push("\\u00" + i.toString(16));
+  }
+})()
+  
 var characters = {};
+characters.encode_char = function(c) { return nchar_to_int[c]; };
+characters.decode_char = function(c) { return int_to_nchar[characters.char_to_int(c)]; };
+characters.encode = function(s) { var r = ""; for(var i = 0; i < s.length; ++i) r += characters.encode_char(s[i]); return r; };
+characters.decode = function(s) { var r = ""; for(var i = 0; i < s.length; ++i) r += characters.decode_char(s[i]); return r; };
+characters.correctify = function(c) { return characters.encode_char(characters.correct(c)); };
+characters.correctifyE = function(c) { return "\\" + characters.encode_char(characters.correct(c)); };
+characters.int_to_char = function(i) { return String.fromCharCode(i); };
+characters.char_to_int = function(i) { return i.charCodeAt(0); };
 characters.regex = {};
-characters.chars = int_to_char;
+characters.chars = [];
+(function(){
+  for(var i = 0; i < 256; ++i) {
+    characters.chars.push(characters.int_to_char(i));
+  }
+})()
 characters.chars.regexified = regexified;
 characters.chars.min = 0;
 characters.chars.max = 255;
-characters.chars.string = int_to_char.join("");
-characters.chars.is = function(c) { return characters.is_valid_char(c) }
-characters.is_valid_code = function(i) { return characters.chars.min <= i && i <= characters.chars.max };
-characters.is_valid_char = function(c) { return char_to_int[c] !== undefined };
-characters.int_to_char = function(i) { return int_to_char[i] };
-characters.char_to_int = function(c) { return char_to_int[c] };
+characters.chars.string = characters.chars.join("");
 
 characters.printables = [];
 characters.printables.regexified = [];
@@ -69,7 +61,7 @@ characters.printables.min = 0;
 characters.printables.max = 97;
 (function(){
   for(var i = characters.printables.min; i <= characters.printables.max; ++i) {
-    characters.printables.push(int_to_char[i]);
+    characters.printables.push(characters.int_to_char(i));
     characters.printables.regexified.push(regexified[i]);
   }
 })();
@@ -77,7 +69,7 @@ characters.printables.string = characters.printables.regexified.join("");
 characters.regex.a_printable = "[" + characters.printables.string + "]";
 characters.regex.not_a_printable = "[^" + characters.printables.string + "]";
 characters.printables.is = function(c) {
-  c = char_to_int[c];
+  c = characters.char_to_int(c);
   return characters.printables.min <= c && c <= characters.printables.max;
 };
 
@@ -87,7 +79,7 @@ characters.compressables.min = 0;
 characters.compressables.max = 195;
 (function(){
   for(var i = characters.compressables.min; i <= characters.compressables.max; ++i) {
-    characters.compressables.push(int_to_char[i]);
+    characters.compressables.push(characters.int_to_char(i));
     characters.compressables.regexified.push(regexified[i]);
   }
 })();
@@ -95,7 +87,7 @@ characters.compressables.string = characters.compressables.regexified.join("");
 characters.regex.a_compressable = "[" + characters.compressables.string + "]";
 characters.regex.not_a_compressable = "[^" + characters.compressables.string + "]";
 characters.compressables.is = function(c) {
-  c = char_to_int[c];
+  c = characters.char_to_int(c);
   return characters.compressables.min <= c && c <= characters.compressables.max;
 };
 
@@ -104,18 +96,19 @@ characters.noncompressables.min = 196;
 characters.noncompressables.max = 255;
 (function(){
   for(var i = characters.noncompressables.min; i <= characters.noncompressables.max; ++i) {
-    characters.noncompressables.push(int_to_char[i]);
+    characters.noncompressables.push(characters.int_to_char(i));
   }
 })();
 characters.noncompressables.string = characters.noncompressables.join("");
 characters.noncompressables.is = function(c) {
-  c = char_to_int[c];
+  c = characters.char_to_int(c);
   return characters.noncompressables.min <= c && c <= characters.noncompressables.max;
 };
 
-characters.tiny_digits = [handleBug('°'),handleBug('¹'),handleBug('²'),
-                          handleBug('³'),handleBug('⁴'),handleBug('⁵'),
-                          handleBug('⁶'),handleBug('⁷'),handleBug('⁸'),handleBug('⁹')];
+characters.tiny_digits = [characters.correctify('°'),characters.correctify('¹'),characters.correctify('²'),
+                          characters.correctify('³'),characters.correctify('⁴'),characters.correctify('⁵'),
+                          characters.correctify('⁶'),characters.correctify('⁷'),characters.correctify('⁸'),
+                          characters.correctify('⁹')];
 characters.tiny_digits.string = characters.tiny_digits.join("");
 characters.regex.a_tiny_digit = "[" + characters.tiny_digits.string + "]";
 characters.regex.not_a_tiny_digit = "[^" + characters.tiny_digits.string + "]";
@@ -150,13 +143,15 @@ characters.num_to_tiny_num = function(s) {
 };
 
 characters.printify_char = function(c) {
-  if(c === characters.correct("¶")) return "\n";
-  if(c === characters.correct("¤")) return " ";
+  if(c === characters.correctify("¶")) return characters.correctify("\n");
+  if(c === characters.correctify("¤")) return characters.correctify(" ");
   return c;
 };
   
 function handleUnicode(s) {
-  return s.replace(/\%([0-9A-F]+)/g, function(c,n) { return String.fromCharCode(parseInt(n, 16)); });
+  s = characters.decode(s);
+  s = s.replace(/\%([0-9A-F]+)/g, function(c,n) { return String.fromCharCode(parseInt(n, 16)); });
+  return characters.encode(s);
 };
   
 characters.printify_string = function(s) {
@@ -167,12 +162,13 @@ characters.printify_string = function(s) {
 };
   
 characters.deprintify_char = function(c) {
+  if(c === characters.correctify("\n")) return characters.correctify("¶");
+  if(c === characters.correctify(" ")) return characters.correctify("¤");
   // Handles unicode characters.
   if(!characters.printables.is(c)) {
-    return "%" + c.charCodeAt(0).toString(16).toUpperCase();
+    c = characters.decode_char(c);
+    return characters.encode("%" + c.charCodeAt(0).toString(16).toUpperCase());
   }
-  if(c === "\n") characters.correct("¶");
-  if(c === " ") characters.correct("¤");
   return c;
 };
   
@@ -376,7 +372,7 @@ characters.compress_occur = function(s) {
   // Now, can assign each character an index based off of the key.
   for(var i = 0, l = s.length; i < l; ++i) {
     // Since there are only 98 printable characters we know that the biggest index will be 97.
-    res += int_to_char[find(key, s[i])];
+    res += characters.int_to_char(find(key, s[i]));
   }
   
   // The result is now made up of the indexes, but we have not actually compressed this depends on the bits.
@@ -394,7 +390,7 @@ characters.decompress_occur = function(key, compressed) {
   var decompressed = "";
   
   for(var i = 0, l = res.length; i < l; ++i) {
-    decompressed += key[char_to_int[res[i]]];
+    decompressed += key[characters.char_to_int(res[i])];
   }
   
   return decompressed;
@@ -406,23 +402,23 @@ characters.compress_range = function(s) {
   var max = 0, min = 97;
   
   for(var i = s.length; i--;) {
-    var v = char_to_int[s[i]];
+    var v = characters.char_to_int(s[i]);
     if(max < v) max = v;
     if(v < min) min = v;
   }
   
   // Now, offset each character based off of the min.
   for(var i = 0, l = s.length; i < l; ++i) {
-    res += int_to_char[char_to_int[s[i]] - min];
+    res += characters.int_to_char(characters.char_to_int(s[i]) - min);
   }
   
   var max_num_bits = (max - min).toString(2).length;
   
   res = characters.compress_bitpack(max_num_bits, res);
   
-  var min_char = characters.bitify_char(int_to_char[min]);
+  var min_char = characters.bitify_char(characters.int_to_char(min));
   min_char.push(min_char.shift());
-  var bits = characters.bitify_char(int_to_char[max_num_bits]);
+  var bits = characters.bitify_char(characters.int_to_char(max_num_bits));
   if(min < 8) {
     min_char[7] = 1;
     min_char[1] = bits[5];
@@ -448,7 +444,7 @@ characters.decompress_range = function(compressed) {
     min_char[3] = 0;
     compressed = compressed.slice(1, compressed.length);
   } else {
-    max_num_bits = char_to_int[compressed[1]];
+    max_num_bits = characters.char_to_int(compressed[1]);
     compressed = compressed.slice(2, compressed.length);
   }
   min_char.unshift(min_char.pop());
@@ -457,7 +453,7 @@ characters.decompress_range = function(compressed) {
   compressed = characters.decompress_bitpack(max_num_bits, compressed);
   var decompressed = "";
   for(var i = 0, l = compressed.length; i < l; ++i) {
-    decompressed += int_to_char[char_to_int[compressed[i]] + min];
+    decompressed += characters.int_to_char(characters.char_to_int(compressed[i]) + min);
   }
   return decompressed;
 };
