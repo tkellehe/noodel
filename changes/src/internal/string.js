36ae720f27608845859c946ee0a52613576179ed
tkellehe <mitchell.kelleher@gmail.com>
2017-08-25 17:41:17 -0400

    Naming and Formating
    Adding some better names to fit the rest of Noodel. Also, corrected the
    constructor of the STRING class.

--- changes/src/internal/string.js
+++ changes/src/internal/string.js
@@ -5,7 +5,9 @@
 function STRING(obj) {
     this.type = "STRING";
     this.is_noodel_type = true;
-    this.value = obj === undefined || obj.toString === undefined ? "" : obj.toString();
+    this.value = obj === undefined ? "" :
+        (obj.is_noodel_type ? obj.to_string() :
+            (typeof obj === "string" ? obj : "")) 
     this.props = {};
 }
 
@@ -13,24 +15,42 @@ function STRING(obj) {
 //////////////////////////////////////////////////////////////////////////////////////////
 /// These functions return the most primitive representation of the type.
 //----------------------------------------------------------------------------------------
-STRING.prototype.toString = function() {
+STRING.prototype.to_string = function() {
     return this.value.toString.apply(this.value, Array.prototype.slice.call(arguments));
 }
 
 
 //----------------------------------------------------------------------------------------
-STRING.prototype.toNumber = function() {
+STRING.prototype.to_number = function() {
+    // Problem with converting to a number...
     return this.value.valueOf.apply(this.value, Array.prototype.slice.call(arguments));
 }
 
 
 //----------------------------------------------------------------------------------------
-STRING.prototype.toArray = function() {
+STRING.prototype.to_array = function() {
     return characters.char_array(this.value);
 }
 //////////////////////////////////////////////////////////////////////////////////////////
 
 
+//////////////////////////////////////////////////////////////////////////////////////////
+/// These are operations that can be done to the Noodel types.
+//----------------------------------------------------------------------------------------
+STRING.prototype.ith_item = function(i) {
+    return new STRING(this.value[i]);
+}
+
+
+//----------------------------------------------------------------------------------------
+STRING.prototype.flip = function() {
+    return new STRING(characters.reverse_string(this.value));
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+/// These functions are basic operators that can be used with other Noodel types.
 //----------------------------------------------------------------------------------------
 STRING.prototype.add_item_onto_the_right = function(item) {
     return new STRING(this.value + item.toString());
@@ -54,6 +74,7 @@ STRING.prototype.remove_item_from_the_right = function(item) {
 STRING.prototype.remove_item_from_the_left = function(item) {
     return new STRING(this.value.replace(item.toString(), ""));
 }
+//////////////////////////////////////////////////////////////////////////////////////////
 
 
 //----------------------------------------------------------------------------------------
