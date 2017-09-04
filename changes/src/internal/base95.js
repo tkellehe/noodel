75360fc1881aa983ef36c7077c36ee36c79b9e96
tkellehe <mitchell.kelleher@gmail.com>
2017-09-03 21:50:54 -0400

    Base95
    Adding base95 conversion such that can convert string literals into
    number literals.

---
+++ changes/src/internal/base95.js
@@ -0,0 +1,35 @@
+(function(global, characters){
+
+
+//----------------------------------------------------------------------------------------
+var to_base95 = function(x) {
+    x = Math.abs(x)
+    if(x == 0)
+        return characters.int_to_char(0)
+    var digits = []
+    while(x) {
+        digits.push(characters.int_to_char(x % 95))
+        x = Math.floor(x /= 95)
+    }
+    digits.reverse()
+    return digits.join("")
+}
+
+
+//----------------------------------------------------------------------------------------
+var from_base95 = function(s) {
+    var x = 0;
+    for(var i = 0, l = s.length; i < l; ++i)
+        x += characters.char_to_int(s[l - i - 1]) * Math.pow(95, i);
+    return x;
+}
+
+
+//----------------------------------------------------------------------------------------
+global.base95 = {
+    to: to_base95,
+    from: from_base95
+}
+
+
+})(this, this.characters)
